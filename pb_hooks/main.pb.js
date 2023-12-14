68c3ca954d346a63426bb69b0516034456d2d297
Nathan Cahill <nathan@nathancahill.com>
2023-12-14 12:13:25 +0100

    hook

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,5 +1,8 @@
 onModelAfterCreate((e) => {
-  console.log(e.model.tableName(), "create");
+  console.log(e.model, "create");
+  const cmd = $os.cmd("ls", "-sl", "/pb/pb_migrations");
+  const output = String.fromCharCode(...cmd.output());
+  console.log(output);
 });
 
 onModelAfterUpdate((e) => {
